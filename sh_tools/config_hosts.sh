b4060b7204df967593d871dc0ad64d145a32f6a4
DimEg <Dmitriy>
2017-03-25 22:01:08 +0300

    Fix log directory

--- sh_tools/config_hosts.sh
+++ sh_tools/config_hosts.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 printf "\n---- Setting up hosts! ----\n"
 IP="$(docker inspect --format '{{ .NetworkSettings.Networks.dockersymfony_default.Gateway }}' dockersymfony_nginx_1)"
 echo  "${IP} symfony.dev" | sudo tee --append /etc/hosts
